ab51ef5e01d23f573cf3ec55b703b822afe2aea0
kaurav83 <puga.vitaliy@gmail.com>
2021-06-26 12:18:41 +0300

    SEO. Добавление meta на страницу

--- pages/[type]/[alias].tsx
+++ pages/[type]/[alias].tsx
@@ -14,12 +14,20 @@ import { TopLevelCategory } from '../../interfaces/page.interface';
 import { TopPageComponent } from '../../page-components';
 
 import { firstLevelMenu } from '../../helpers/helpers';
+import Head from 'next/head';
 // const firstCategory = 0;
 
 const TopPage = ({ firstCategory, page, products }: TopPageProps): JSX.Element => {
     
     return (
         <>
+            <Head>
+                <title>{page.metaTitle}</title>
+                <meta name="description" content={page.metaDescription} />
+                <meta property="og:title" content={page.metaTitle} />
+                <meta property="og:description" content={page.metaDescription} />
+                <meta property="og:type" content="article" />
+            </Head>
             {
                 page && products && <>
                     <TopPageComponent 
